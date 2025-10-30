db7df4f2d8894f92c75816d145d219735ebb779d
ahmed-alseidy <ahmedalseidy4@gmail.com>
2025-10-30 15:47:01 +0300

    feat(standards): add standards pages

---
+++ src/app/dashboard/standards/institutional/columns.tsx
@@ -0,0 +1,76 @@
+import type { ColumnDef } from "@tanstack/react-table";
+import { EllipsisVertical, Pencil, Trash } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+export type InstitutionalStandard = {
+  standard: string;
+  practice: string;
+  indicator: string;
+};
+
+export const columns: ColumnDef<InstitutionalStandard>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+  },
+  {
+    accessorKey: "practice",
+    header: "Actions",
+  },
+  {
+    accessorKey: "standard",
+    header: "Standard",
+  },
+  {
+    accessorKey: "indicator",
+    header: "Indicator",
+  },
+  {
+    accessorKey: "actions",
+    header: "",
+    cell: ({ row }) => {
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <EllipsisVertical className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem>
+              <Pencil />
+              Edit
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Trash />
+              Delete
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
---
+++ src/app/dashboard/standards/program/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { Plus } from "lucide-react";
+import { DataTable } from "@/components/data-table";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { columns } from "./columns";
+
+const data = [
+  {
+    standard: "Standard 1",
+    indicator: "Indicator 1",
+    practice: "Practice 1",
+  },
+  {
+    standard: "Standard 2",
+    indicator: "Indicator 2",
+    practice: "Practice 2",
+  },
+];
+
+export default function InstitutionalStandardPage() {
+  return (
+    <div className="space-y-8">
+      <h1 className="text-2xl font-bold text-center">Program Standard</h1>
+      <div className="max-w-3xl mx-auto space-y-4">
+        <div className="flex justify-end">
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant="default">
+                <Plus className="h-4 w-4" />
+                Add Standard
+              </Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Add Standard</DialogTitle>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
+        </div>
+        <DataTable columns={columns} data={data} />
+      </div>
+    </div>
+  );
+}
